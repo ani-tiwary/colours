d298d97ce5ed02edf09e4b4c7da072a8b4a347ba
ani tiwary <anitiwary87@gmail.com>
2025-06-01 17:20:53 -0400

    boilerplate

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold mb-4">Temporary Title</h1>
+      <p>This is just a placeholder page for now.</p>
+    </main>
+  );
+} 
